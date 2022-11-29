e7d7b16e7484abe1bb7a2d34a63f4ee6be6ebce6
RaffyRod <elraffy3@gmail.com>
2022-11-29 01:26:42 -0400

    reporter added

--- cypress.config.js
+++ cypress.config.js
@@ -7,10 +7,24 @@ module.exports = defineConfig({
     baseUrl: 'https://reqres.in',
     usersEnd: '/api/users?page=2'
 
+  },reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    reportTitle: 'API testing report',
+    reportPageTitle: 'API Test Automation Results',
+    reportFilename: 'apiReports',
+    embeddedScreenshots: true,
+    charts: true,
+    inline: true,
+    overwrite: false,
+    autoOpen: false,
+    showPending: false,
+    showSkipped: true,
+    timestamp: "longDate",
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
